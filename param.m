68e6b77bbdaa0e3e5b10663594c4531f2fed970e
Sam Marrett <e0957911@u.nus.edu>
2026-02-23 19:39:08 +0800

    push

--- param.m
+++ param.m
@@ -3,8 +3,8 @@
 weight_distribution = 50.95; %front bias
 
 %% Unsprung mass
-front_unsprung_mass = 12.15;
-rear_unsprung_mass = 13.35;
+frontunsprung.m = 12.15;
+rearunsprung.m = 13.35;
 
 %% Car/ Sprung (Appendix A: diagram of car parameters)
 car.wheelbase = 1.558;
@@ -16,7 +16,7 @@ car.a = car.wheelbase/2 - car.wheelbase*(weight_distribution/100 - 0.5);
 car.b = car.wheelbase-car.a;
 
 % Assume sprung corner weight is equally distributed
-car.m_sprung = (car.m - 2*front_unsprung_mass - 2*rear_unsprung_mass)/4;
+car.m_sprung = (car.m - 2*frontunsprung.m - 2*rearunsprung.m)/4;
 
 %% Suspension Stiffness 
 front.ks = 225 * 175.12684; % spring rate
@@ -29,3 +29,7 @@ front.kw = front.ks*front.MR^2; % wheel rate
 rear.kw = rear.ks*rear.MR^2;
 front.Kr = (front.kw*car.Kt) / (front.kw+car.Kt); %ride rate
 rear.Kr = (rear.kw*car.Kt) / (rear.kw+car.Kt);
+
+%% Natural Frequency and Critical Damping
+frontsprung.omega = (1/(2*pi)) * sqrt(front.Kr/car.m_sprung);
+rearsprung.omega =  (1/(2*pi)) * sqrt(rear.Kr/car.m_sprung);
