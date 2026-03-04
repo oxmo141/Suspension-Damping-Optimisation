f7f87be32d08073eb9606f30907a3f9b5192060a
Sam <Sam@SamMarrett>
2026-03-04 13:27:44 +0800

    one

---
+++ onedegreeoffreedom.m
@@ -0,0 +1,32 @@
+%% 1/8 car model, simplest model of body motion: Appendix C
+% assumption that the unsprung mass is insignificant compared to the sprung
+% mass of each corner.
+
+%% Parameters
+param;
+front.cs = 600; % [Ns/m]
+rear.cs = 1600;
+
+%% Damping Coeff. and Natural Frequency
+sus.front.axle      = front;
+sus.front.unsprung  = frontunsprung;
+sus.front.sprung    = frontsprung;
+
+sus.rear.axle       = rear;
+sus.rear.unsprung   = rearunsprung;
+sus.rear.sprung     = rearsprung;
+
+susp = fieldnames(sus);
+results = struct();
+
+for j = 1:length(susp)
+    
+    axl = sus.(susp{j}).axle;
+    us  = sus.(susp{j}).unsprung;
+    s   = sus.(susp{j}).sprung;
+
+    results.(susp{j}).damping_ratio = ...
+        axl.cs / (2*sqrt(axl.kw/s.m));
+    results.(susp{j}).omega_natural = sqrt(axl.kw/s.m);
+    
+end
